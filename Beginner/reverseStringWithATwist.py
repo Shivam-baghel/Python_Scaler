d67d75023689539fe717f162842b39561e57dc9c
Shivam-baghel <shivambaghel378@gmail.com>
2022-06-24 04:19:32 +0530

    finally done reverseStringwithATwist.

--- Beginner/reverseStringWithATwist.py
+++ Beginner/reverseStringWithATwist.py
@@ -7,42 +7,45 @@ output_string = 'Shivam is name My'
 
 """
 
-# def reverseString(string):
-#     # reverse string without using empty string
+def reverseString(string):
+    # reverse string without using empty string
 
-#     lastIndex = len(string)-1
-#     # we will be swapping the individual characters one by one
-#     for index in range(0,len(string)//2): #we will go from 0 to half the length of entire list
-#         tempString = string[index]          # storing the character to tempString
-#         string[index] = string[lastIndex]   # swapping the first character with last character
-#         string[lastIndex] = tempString
-#         lastIndex -=1                       #Making sure the last index is updated every time we make a swap
-#     return string
+    lastIndex = len(string)-1
+    # we will be swapping the individual characters one by one
+    for index in range(0,len(string)//2): #we will go from 0 to half the length of entire list
+        tempString = string[index]          # storing the character to tempString
+        string[index] = string[lastIndex]   # swapping the first character with last character
+        string[lastIndex] = tempString
+        lastIndex -=1                       #Making sure the last index is updated every time we make a swap
+    return string
 
-# def reverseWords(string,firstIndex,lastIndex):
-#     lIndex = lastIndex
-#     for index in range(firstIndex,lastIndex//2):
-#         tempString = string[index]          # storing the character to tempString
-#         string[index] = string[lIndex]   # swapping the first character with last character
-#         string[lIndex] = tempString
-#         lIndex -=1
-#     return string
+def reverseWords(string,firstIndex,lastIndex):
+    lIndex = lastIndex
+    while firstIndex<lIndex:
+        tempString = string[firstIndex]          # storing the character to tempString
+        string[firstIndex] = string[lIndex]   # swapping the first character with last character
+        string[lIndex] = tempString
+        lIndex -=1
+        firstIndex +=1
+    return string
  
 
-# inputString = list(input())
-# startIndex = 0
-# for index in range(len(inputString)):
+inputString = list(input())
+startIndex = 0
+for index in range(len(inputString)+1):
     
-#     if inputString[index] == " ":
-#         print(index,inputString[index])
-#         string = reverseWords(inputString,startIndex,index-1)
-#         startIndex = index+1
+    if index == len(inputString) or inputString[index] == " ":
+        # print(index,inputString[index])
+        string = reverseWords(inputString,startIndex,index-1)
+        startIndex = index+1
 
-# # revString = reverseString(string)
+revString = reverseString(string)
 
-# # print(revString)
+print(" ".join(revString))
 
 # print(string)
+
+"""
 
 x = input("Enter any string: ")
 #take input from user
@@ -56,4 +59,6 @@ a.reverse()
 print(a)
 print(' '.join(a))
 #concatenate them into a string
+
+"""
 
