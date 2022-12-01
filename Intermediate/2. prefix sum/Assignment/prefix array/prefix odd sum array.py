16a5e06150390adaa9da7f899c35eeba180309bf
shivam-baghel <shivambaghel378@gmail.com>
2022-12-01 14:24:11 +0530

    made change to improve on prefix odd sum array

--- Intermediate/2. prefix sum/Assignment/prefix array/prefix odd sum array.py
+++ Intermediate/2. prefix sum/Assignment/prefix array/prefix odd sum array.py
@@ -3,7 +3,7 @@
 
 arr = tuple(map(int,input().split()))       # accepting the array
 prefixOdd = [0] * len(arr)          # creating a new different array for prefixOdd array
-prefixOdd[0] = arr[0]           # make the first element of the prefixOdd array equal to the first element of the array.
+prefixOdd[0] = 0          # make the first element of the prefixOdd array equal to the 0. As 0th index is even and we don't have any previous element which we can use to determine 0th index value.
 
 for i in range(1,len(arr)):
     if i % 2 != 0:              # populating the prefixodd array with odd index values only.
