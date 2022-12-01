266fa82799d822796ec142f183847e91ec2f629f
shivam-baghel <shivambaghel378@gmail.com>
2022-12-01 12:25:50 +0530

    started to add pdf too.

--- Intermediate/2. prefix sum/Equilibrium index/special equilibrium index of an array.py
+++ Intermediate/2. prefix sum/Equilibrium index/special equilibrium index of an array.py
@@ -36,3 +36,90 @@ Removing arr[2] from the given array modifies arr[] to { 1, 1 } such that arr[0]
 Therefore, the required output is 3.
 
 """
+
+def sumOfEven(evenArray:list,startIndex:int,endIndex:int):
+    """this array return the sumation of odd index's of the array excluding the given index.
+
+    Args:
+        oddArray (list): prefix odd sum array.
+        startIndex (int): starting index
+        endIndex (int): ending index
+
+    Returns:
+        Integer: returns the outpupt
+    """
+    
+    if startIndex > 0:
+        return evenArray[endIndex-1]-evenArray[startIndex]
+    else:
+        return evenArray[endIndex-1]
+    
+def sumOfOdd(oddArray:list,startIndex:int,endIndex:int):
+    """this array return the sumation of odd index's of the array excluding the given index.
+
+    Args:
+        oddArray (list): prefix odd sum array.
+        startIndex (int): starting index
+        endIndex (int): ending index
+
+    Returns:
+        Integer: returns the output 
+    """
+    
+    if startIndex > 0:
+        return oddArray[endIndex-1]-oddArray[startIndex]
+    else:
+        return oddArray[endIndex-1]
+    
+def specialEquibliriumIndex(array:list):
+    """ This function will for every index. If that index is special index then the count will increase.
+
+    Args:
+        array (list): list of intergers.
+    Return:
+        return: the count of special index's present in the given list of integers.
+        
+    """
+    
+    length_of_Array = len(array)
+    
+    # first create prefix even sum array.
+    
+    prefix_even_sum = [0]*length_of_Array
+    prefix_even_sum[0] = array[0]
+    
+    for index,value in enumerate(array):
+        # Bit manipulation way to check if value is even or odd.
+        if index&1 == 0:
+            prefix_even_sum[index] = prefix_even_sum[index-1] + value
+        else:
+            prefix_even_sum[index] = prefix_even_sum[index-1]
+            
+    # create prefix odd sum array. 
+    
+    prefix_odd_sum = [0] * length_of_Array
+    prefix_odd_sum[0] = array[0]
+    
+    for index,value in enumerate(array):
+        # Bit manipulation way to check jif value is even or odd.
+        if index & 1 == 1:
+            prefix_odd_sum[index] = prefix_odd_sum[index-1] + value
+        else:
+            prefix_odd_sum[index] = prefix_odd_sum[index-1]
+            
+    
+    # create a loop for checking if the paricular index is specilised index or not.
+    count = 0
+    for index,value in enumerate(array):
+        
+        # check if a index is a special index or not.
+        sumEven = sumOfEven(prefix_even_sum,0,index)+sumOfOdd(prefix_even_sum,index,length_of_Array)
+        sumOdd = sumOfOdd(prefix_odd_sum,0,index)+sumOfEven(prefix_odd_sum,index,length_of_Array)
+        
+        if sumEven == sumOdd:
+            count +=1
+    
+    return count
+
+A=[2, 1, 6, 4]
+print(specialEquibliriumIndex(A))
