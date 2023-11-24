815cc0b9a8397ab9a75bad8bbace3ff378b6ea54
Shivam-baghel <shivambaghel378@gmail.com>
2023-11-25 02:36:45 +0530

    just ran the code

--- 2. Data Structures and Algorithm/1. Intermediate/05. Arrays - 2D Matrices/Homework/Q2. Matrix Scalar Product.py
+++ 2. Data Structures and Algorithm/1. Intermediate/05. Arrays - 2D Matrices/Homework/Q2. Matrix Scalar Product.py
@@ -54,9 +54,17 @@ Explanation 2:
 ==> [[5]]
 """
 
-def matrixProduct(self, A, B):
-    for i in range(len(A)):
-        for j in range(len(A[i])):
-            A[i][j] = A[i][j]*B
+def matrixProduct(A,B):
+     for i in range(len(A)):
+          for j in range(len(A[i])):
+               A[i][j] = A[i][j]*B
 
-    return A
+     return A
+
+
+A = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+B = 2 
+
+print(matrixProduct(A,B))
