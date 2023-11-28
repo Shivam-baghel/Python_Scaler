21d38325521741fbe5ea00495cc9d74efe74a80a
Shivam-baghel <shivambaghel378@gmail.com>
2023-11-28 18:08:44 +0530

    solved 2d matrices assignment question

---
+++ 2. Data Structures and Algorithm/1. Intermediate/05. Arrays - 2D Matrices/Assignment/Q3. Column Sum.py
@@ -0,0 +1,53 @@
+""" 
+Q3. Column Sum
+
+You are given a 2D integer matrix A, return a 1D integer array containing column-wise sums of original matrix.
+
+Problem Constraints
+1 <= A.size() <= 103
+1 <= A[i].size() <= 103
+1 <= A[i][j] <= 103
+
+Input Format
+First argument is a 2D array of integers.(2D matrix).
+
+Output Format
+Return an array containing column-wise sums of original matrix.
+
+Example Input
+Input 1:
+
+[1,2,3,4]
+[5,6,7,8]
+[9,2,3,4]
+
+
+Example Output
+Output 1:
+
+{15,10,13,16}
+
+
+Example Explanation
+Explanation 1
+
+Column 1 = 1+5+9 = 15
+Column 2 = 2+6+2 = 10
+Column 3 = 3+7+3 = 13
+Column 4 = 4+8+4 = 16
+"""
+
+def column_sum( A):
+    lengthOfColumn = len(A[0])
+    lengthOfRow = len(A)
+    res = []
+    for j in range(lengthOfColumn):
+        sum = 0
+        for i in range(lengthOfRow):
+            sum = sum + A[i][j]
+
+        res.append(sum)
+
+    return res
+        
+        
---
+++ 2. Data Structures and Algorithm/1. Intermediate/05. Arrays - 2D Matrices/Assignment/Q8. Rotate Matrix.py
@@ -0,0 +1,93 @@
+""" 
+Q8. Rotate Matrix
+
+You are given a n x n 2D matrix A representing an image.
+Rotate the image by 90 degrees (clockwise).
+You need to do this in place.
+
+Note: If you end up using an additional array, you will only receive partial score.
+
+Problem Constraints
+1 <= n <= 1000
+
+Input Format
+First argument is a 2D matrix A of integers
+
+Output Format
+Return the 2D rotated matrix.
+
+Example Input
+Input 1:
+
+ [
+    [1, 2],
+    [3, 4]
+ ]
+Input 2:
+
+ [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+ ]
+
+
+Example Output
+Output 1:
+
+ [
+    [3, 1],
+    [4, 2]
+ ]
+Output 2:
+
+ [
+    [7, 4, 1],
+    [8, 5, 2],
+    [9, 6, 3]
+ ]
+
+
+Example Explanation
+Explanation 1:
+
+ After rotating the matrix by 90 degree:
+ 1 goes to 2, 2 goes to 4
+ 4 goes to 3, 3 goes to 1
+Explanation 2:
+
+ After rotating the matrix by 90 degree:
+ 1 goes to 3, 3 goes to 9
+ 2 goes to 6, 6 goes to 8
+ 9 goes to 7, 7 goes to 1
+ 8 goes to 4, 4 goes to 2
+"""
+
+def rotate_matrix( A):
+
+    # # for anti clockwise
+    # # idea here is first we have to transpose the matrix and then we have swap first row with last row. then second row
+    # # with second last row and soo on.
+
+    # # To Transpose matrix
+    # n = len(A)
+    # for i in range(n):
+    #     for j in range(i):
+    #         A[i][j],A[j][i] = A[j][i],A[i][j]
+    
+
+    # # To swap rows
+
+    # for i in range(n//2):
+    #     for j in range(n):
+    #         A[i][j],A[n-1-i][j] = A[n-1-i][j],A[i][j]
+
+    # return A
+
+    n = len(A)
+    for i in range(1, n) :
+        for j in range(i) :
+            A[i][j], A[j][i] = A[j][i], A[i][j]
+    for i in A :
+        i.reverse()
+    return A
