3f189b0e80cbbd74cc076ac4d577a8349ba8086d
SHIVAM <shivambaghel378@gmail.com>
2023-11-30 17:46:42 +0530

    did few modification.

--- 2. Data Structures and Algorithm/2. Advance/08. Maths - Prime Numbers/Assignment/Lucky Numbers.py
+++ 2. Data Structures and Algorithm/2. Advance/08. Maths - Prime Numbers/Assignment/Lucky Numbers.py
@@ -1,5 +1,4 @@
-
-""" 
+"""
 Q3. Lucky Number
 A lucky number is a number that has exactly 2 distinct prime divisors.
 You are given a number A, and you need to determine the count of lucky numbers between the range 1 to A (both inclusive).
@@ -31,28 +30,29 @@ Explanation 2:
  Between [1, 12] there are 3 lucky number: 6, 10 and 12.
 """
 
+
 def luckyNumber(A):
     num = A
-    primeArray = [0]*(num+1)
+    primeArray = [0] * (num + 1)
     """ we have created a primeArray of size A+1. this array we are creating with the seive of erthotheisies.
     here we have made some modification to array. instead of making multiples of prime no. false. we will increase the count of that particular no. this will indicate that particular no. is divisible by a prime no. or more prime numbers.
     after this let's say primeArray[6] == 2 that means 6 is divisible by two different prime no.
     now we just have to count the no. of 2's in prime array that is out ans.
     """
-    for i in range(2,num+1):
+    for i in range(2, num + 1):
         # we interate on multiples of i if i is prime number
         if (primeArray[i] == 0):
             # i is prime. Iterate on multiples of i till n.
-            for j in range(2*i,num+1,i):
-                primeArray[j] += 1 
-                
+            for j in range(2 * i, num + 1, i):
+                primeArray[j] += 1
+
     ans = 0
     for k in range(len(primeArray)):
-        if primeArray[k]==2:
-            ans +=1
-    
-    return ans            
-        
+        if primeArray[k] == 2:
+            ans += 1
+
+    return ans
+
 
 inputNumber = int(input())
 print(luckyNumber(inputNumber))
--- 2. Data Structures and Algorithm/2. Advance/08. Maths - Prime Numbers/Assignment/Prime Numbers-Seive of Erthotheisis.py
+++ 2. Data Structures and Algorithm/2. Advance/08. Maths - Prime Numbers/Assignment/Prime Numbers-Seive of Erthotheisis.py
@@ -1,24 +1,29 @@
-""" 
+"""
+Seive of Erthotheisis
 Print all prime nombers from 1 - N.
 ex:
 n = 10
 output: 2,3,5,7
 """
+
+
 def allPrime(num):
-    primeArray = [True]*(num+1)
+    primeArray = [True] * (num + 1)
     primeArray[1] = False
     primeArray[0] = False
-    
-    for i in range(2,num+1):
+
+    for i in range(2, num + 1):
         # we interate on multiples of i if i is prime number
         if (primeArray[i] == True):
             # i is prime. Interate on multiples of i till n.
-            for j in range(2*i,num+1,i):
+            for j in range(2 * i, num + 1, i):
                 primeArray[j] = False
-    
-    for k in range(2,num+1):
+
+    for k in range(2, num + 1):
         if primeArray[k] == True:
-            print(k,end=" ")
-            
-number = int(input())
-allPrime(number)
+            print(k, end=" ")
+
+
+if __name__ == '__main__':
+    number = int(input())
+    allPrime(number)
