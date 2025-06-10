61cdadaeb57616dbb7762aa86637b732ae21d39c
Shivam-baghel <shivambaghel378@gmail.com>
2025-06-10 06:35:19 +0530

    just formatted in vscode to if git is working

--- 2. Data Structures and Algorithm/1. Intermediate/01. Introduction to arrays/check pairsum.py
+++ 2. Data Structures and Algorithm/1. Intermediate/01. Introduction to arrays/check pairsum.py
@@ -1,5 +1,5 @@
 """
-Given N array element , check if there exist a pair (i,j) such that 
+Given N array element , check if there exist a pair (i,j) such that
 arr[i] + arr[j] == k && i!=j
 Note: i and j are index values.
 
@@ -8,7 +8,7 @@ Hint: whenever there is check present in the question this means that return typ
 
 # Brute force method
 
-from operator import truediv
+# from operator import truediv
 
 
 def checkSum(arr, k):
@@ -26,9 +26,12 @@ def checkSum(arr, k):
 # here we know that we have to check every single possible pair combination of i and j.
 # but many of those combination will be repeated as (i,j) and (j,i) are same pair.
 
+
 def checkSum2(arr, k):
     for i in range(len(arr)):
-        for j in range(i):  # here we are only going till i-1. so repeated same pair won't be comming across.
+        for j in range(i):
+            # here we are only going till i-1. so repeated same pair won't be comming across.
+
             if arr[i] + arr[j] == k:
                 return True
 
