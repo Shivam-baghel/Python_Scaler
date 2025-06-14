6b6c99bbd3b0f915b99d50053c79220a05eab48d
Shivam-baghel <shivambaghel378@gmail.com>
2025-06-14 12:30:31 +0530

    renaming

--- 2. Data Structures and Algorithm/1. Intermediate/01. Introduction to arrays/count.py
+++ 2. Data Structures and Algorithm/1. Intermediate/01. Introduction to arrays/count.py
@@ -4,23 +4,23 @@ Given N array elements, count no. of element having atleast 1 element greater th
 EX :
 a = [-3,-2,1,4,3,8,5,8]
 ans = 6 as atleast 6 element have 1 element greater than them.
-as 8 is two times 
+as 8 is two times
 
 """
 
-from cmath import inf
+# from cmath import inf 
 
 
 def solve(a):
-    maxVal = -float('inf')
+    maxValue = -float("inf")
     for i in range(len(a)):
-        if maxVal < a[i]:
-            maxVal = a[i]
+        if maxValue < a[i]:
+            maxValue = a[i]
 
     count = 0
 
     for i in range(len(a)):
-        if a[i] != maxVal:
+        if a[i] != maxValue:
             count += 1
 
     return count
--- 2. Data Structures and Algorithm/1. Intermediate/02. Arrays - Prefix sum/Assignment/prefix array/prefix odd sum array.py
+++ 2. Data Structures and Algorithm/1. Intermediate/02. Arrays - Prefix sum/Assignment/prefix array/prefix odd sum array.py
@@ -1,8 +1,7 @@
 
 arr = tuple(map(int ,input().split()))       # accepting the array
 prefixOdd = [0] * len(arr)          # creating a new different array for prefixOdd array
-prefixOdd
-    [0] = 0          # make the first element of the prefixOdd array equal to the 0. As 0th index is even and we don't have any previous element which we can use to determine 0th index value.
+prefixOdd[0] = 0          # make the first element of the prefixOdd array equal to the 0. As 0th index is even and we don't have any previous element which we can use to determine 0th index value.
 
 for i in range(1 ,len(arr)):
     if i % 2 != 0:              # populating the prefixodd array with odd index values only.
@@ -12,3 +11,6 @@ for i in range(1 ,len(arr)):
 
 print(arr)
 print(prefixOdd)            # printing the prefixOdd array
+
+
+
