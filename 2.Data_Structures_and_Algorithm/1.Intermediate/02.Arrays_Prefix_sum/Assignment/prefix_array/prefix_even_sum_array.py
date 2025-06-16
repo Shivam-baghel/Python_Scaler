d180ecc05c552c81ba04bb26a7b5f9a5b19199c2
Shivam-baghel <shivambaghel378@gmail.com>
2025-06-16 23:25:34 +0530

    Restructured the file

--- 2.Data_Structures_and_Algorithm/1.Intermediate/02.Arrays_Prefix_sum/Assignment/prefix_array/prefix_even_sum_array.py
+++ 2.Data_Structures_and_Algorithm/1.Intermediate/02.Arrays_Prefix_sum/Assignment/prefix_array/prefix_even_sum_array.py
@@ -1,15 +1,25 @@
-arr = list(map(int, input().split()))  # accepting the array.
+# accepting the array.
 
-prefixEven = [0] * len(arr)  # creating a new dirrerent array for prefixEven array.
+# arr = list(map(int, input().split()))
+def prefixEvenSum(arr):
+    prefixEven = [0] * len(arr)  # creating a new dirrerent array for prefixEven array.
 
-# makeing the first element of the prefixEven array equal to the first element of the array.
-prefixEven[0] = arr[0]
+    # makeing the first element of the prefixEven array equal to the first element of the array.
+    prefixEven[0] = arr[0]
 
-for i in range(1, len(arr)):
-    if i % 2 == 0:  # populating the prefixEven array with even index values only.
-        prefixEven[i] = prefixEven[i - 1] + arr[i]
-    else:
-        prefixEven[i] = prefixEven[i - 1]
+    for i in range(1, len(arr)):
+        if i % 2 == 0:  # populating the prefixEven array with even index values only.
+            prefixEven[i] = prefixEven[i - 1] + arr[i]
+        else:
+            prefixEven[i] = prefixEven[i - 1]
 
-print(arr)
-print(prefixEven)  # p rinting the prefixEven values.
+    print(arr)
+
+    # printing the prefixEven values.
+
+    print(prefixEven)
+
+
+if __name__ == "__main__":
+    array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    prefixEvenSum(array)
--- 2.Data_Structures_and_Algorithm/1.Intermediate/02.Arrays_Prefix_sum/Assignment/prefix_array/prefix_odd_sum_array.py
+++ 2.Data_Structures_and_Algorithm/1.Intermediate/02.Arrays_Prefix_sum/Assignment/prefix_array/prefix_odd_sum_array.py
@@ -1,16 +1,20 @@
+# arr = tuple(map(int ,input().split()))       # accepting the array
+def prefixOddSum(arr):
+    prefixOdd = [0] * len(arr)  # creating a new different array for prefixOdd array
+    prefixOdd[0] = (
+        0  # make the first element of the prefixOdd array equal to the 0. As 0th index is even and we don't have any previous element which we can use to determine 0th index value.
+    )
 
-arr = tuple(map(int ,input().split()))       # accepting the array
-prefixOdd = [0] * len(arr)          # creating a new different array for prefixOdd array
-prefixOdd[0] = 0          # make the first element of the prefixOdd array equal to the 0. As 0th index is even and we don't have any previous element which we can use to determine 0th index value.
+    for i in range(1, len(arr)):
+        if i % 2 != 0:  # populating the prefixodd array with odd index values only.
+            prefixOdd[i] = prefixOdd[i - 1] + arr[i]
+        else:
+            prefixOdd[i] = prefixOdd[i - 1]
 
-for i in range(1 ,len(arr)):
-    if i % 2 != 0:              # populating the prefixodd array with odd index values only.
-        prefixOdd[i] = prefixOdd[ i -1] + arr[i]
-    else:
-        prefixOdd[i] = prefixOdd[ i -1]
-
-print(arr)
-print(prefixOdd)            # printing the prefixOdd array
+    print(arr)
+    print(prefixOdd)  # printing the prefixOdd array
 
 
-
+if __name__ == "__main__":
+    array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    prefixOddSum(array)
