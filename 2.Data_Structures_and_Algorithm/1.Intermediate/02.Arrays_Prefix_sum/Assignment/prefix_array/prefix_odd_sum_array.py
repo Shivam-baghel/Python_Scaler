c46c48f5e9203580a5f3eebd39d806af7381e85c
Shivam-baghel <shivambaghel378@gmail.com>
2025-06-16 23:37:21 +0530

    changed the comments

--- 2.Data_Structures_and_Algorithm/1.Intermediate/02.Arrays_Prefix_sum/Assignment/prefix_array/prefix_odd_sum_array.py
+++ 2.Data_Structures_and_Algorithm/1.Intermediate/02.Arrays_Prefix_sum/Assignment/prefix_array/prefix_odd_sum_array.py
@@ -1,13 +1,19 @@
 # arr = tuple(map(int ,input().split()))       # accepting the array
 def prefixOddSum(arr):
-    prefixOdd = [0] * len(arr)  # creating a new different array for prefixOdd array
-    prefixOdd[0] = (
-        0  # make the first element of the prefixOdd array equal to the 0. As 0th index is even and we don't have any previous element which we can use to determine 0th index value.
-    )
+    # creating a new different array for prefixOdd array
+
+    prefixOdd = [0] * len(arr)
+
+    # make the first element of the prefixOdd array equal to the 0. As 0th index is even and we don't have any previous element which we can use to determine 0th index value.
+
+    prefixOdd[0] = 0
 
     for i in range(1, len(arr)):
-        if i % 2 != 0:  # populating the prefixodd array with odd index values only.
+        # populating the prefixodd array with odd index values only.
+
+        if i % 2 != 0:
             prefixOdd[i] = prefixOdd[i - 1] + arr[i]
+
         else:
             prefixOdd[i] = prefixOdd[i - 1]
 
